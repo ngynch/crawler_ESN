3bfc6516b36f9902bcc09c304a7a6f2a9755b2f7
cnguyen <conghuannguyen1996@gmail.com>
2022-08-13 11:21:39 +0200

    Set up cronjobs every 5minutes and stop scripts if script was successful or exception occured

--- script.py
+++ script.py
@@ -7,6 +7,9 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import time
 import configparser
+import sys
+import os
+import traceback
 
 def get_html_source(link:str) -> str:
     """get the html source code of a link
@@ -51,17 +54,41 @@ def evaluate_html_esn(page_source: str) -> Dict[str,bool]:
     return products_available
 
 
-def send_email(products: Dict[str, bool]):
+def send_products_email(products: Dict[str, bool]):
+    os.system('crontab -r')
+    print("STOPPED CRONTABS")
     config = configparser.ConfigParser()
     config.read("config.ini")
     sender_email = config["EMAIL"]["account"]
     pw = config["EMAIL"]["password"]
     msg = MIMEMultipart()
     msg['From'] = sender_email
-    msg['Subject'] = "greif zu bruder"
+    msg['Subject'] = "greif zu bruder, script offline now"
     message = '\n'.join(str(products)[1:-1].split(', '))
     msg.attach(MIMEText(message))
-    
+    print(message)
+    port = 465
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.mail.yahoo.com", port, context=context) as server:
+        server.login(sender_email, pw)
+        for receiver in config["EMAIL"]["receivers"].split(','):
+            msg['To'] = receiver
+            server.sendmail(sender_email, receiver, msg.as_string())
+            print(f"Email sent to  {receiver} successfully")
+            time.sleep(2)
+
+def send_error_email(err: str):
+    os.system('crontab -r')
+    print("STOPPED CRONTABS")
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    sender_email = config["EMAIL"]["account"]
+    pw = config["EMAIL"]["password"]
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['Subject'] = "Script failed, check container"
+    message = err
+    msg.attach(MIMEText(message))
     port = 465
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.mail.yahoo.com", port, context=context) as server:
@@ -78,8 +105,15 @@ def main():
     html = get_html_source(link)
     products = evaluate_html_esn(html)
     if any(products.values()):
-        send_email(products)
+        send_products_email(products)
+        sys.exit(0)
     else:
         print("No products available!")
+        sys.exit(0)
 if __name__ == "__main__":
-    main()  
+    try:
+        main()
+    except Exception:
+        send_error_email(traceback.format_exc())
+        traceback.print_exc()
+  
